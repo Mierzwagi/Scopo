1fca9f730a47fa0e1c75a392165104d9dd445cbd
Seu Nome <seu@email.com>
2023-12-01 17:36:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1
+    }
+
+    document.getElementById('radio' + cont).checked = true
+
+}
+
+
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection());
+      },
+    },
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
